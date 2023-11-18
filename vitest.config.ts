bcfd059a4fc34565f0ad8fcaf0b53948e5f6e9aa
Daniel Kaiser <danielkaiser@fastmail.com>
2023-11-18 19:47:15 +0100

    Add test setup.

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./test/setup.ts",
+    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
+    coverage: {
+      provider: "v8", // or 'istanbul'
+      reporter: ["text", "lcov", "html"],
+    },
+  },
+});
