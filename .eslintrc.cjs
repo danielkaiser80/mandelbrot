54a61e06ea5211fee3c2af0be6e9cde9353d7eb6
Daniel Kaiser <danielkaiser@fastmail.com>
2023-11-19 22:03:20 +0100

    Add MUI.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,7 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
+    "react-hooks/exhaustive-deps": "warn"
   },
   settings: {
     react: {
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+
 import "./index.css";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
